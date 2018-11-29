21087849582affca4ba59fc3cdfaa932c719b094
Andrew McLagan <andrewmclagan@gmail.com>
2018-11-29 15:31:48 +1100

    Functionl tests

---
+++ tests/Functional/RedCrossAusTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Functional;
+
+use DiDom\Document;
+use EthicalJobs\Sanitize\PurifierFactory;
+
+class RedCrossAusTest extends \Tests\TestCase
+{    
+    /**
+     * @test
+     */
+    public function it_correctly_purifies_fixture_1()
+    {
+        $before = file_get_contents(__DIR__.'/../Fixtures/RedCrossAus/fixture.1.before.html');
+
+        $after = file_get_contents(__DIR__.'/../Fixtures/RedCrossAus/fixture.1.after.html');
+
+        $purifier = PurifierFactory::create();
+
+        $purified = $purifier->purify($before);
+
+        $this->assertEquals($purified, $after);
+    }   
+
+    /**
+     * @test
+     */
+    public function it_correctly_purifies_fixture_2()
+    {
+        $before = file_get_contents(__DIR__.'/../Fixtures/RedCrossAus/fixture.2.before.html');
+
+        $after = file_get_contents(__DIR__.'/../Fixtures/RedCrossAus/fixture.2.after.html');
+
+        $purifier = PurifierFactory::create();
+
+        $purified = $purifier->purify($before);
+
+        $this->assertEquals($purified, $after);
+    }       
+
+    /**
+     * @test
+     */
+    public function it_correctly_purifies_fixture_3()
+    {
+        $before = file_get_contents(__DIR__.'/../Fixtures/RedCrossAus/fixture.3.before.html');
+
+        $after = file_get_contents(__DIR__.'/../Fixtures/RedCrossAus/fixture.3.after.html');
+
+        $purifier = PurifierFactory::create();
+
+        $purified = $purifier->purify($before);
+
+        $this->assertEquals($purified, $after);
+    }           
+}
