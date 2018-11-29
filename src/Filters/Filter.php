a63d4656e570cfbb665e986bc6f72f524465d24b
Andrew McLagan <andrewmclagan@gmail.com>
2018-11-29 11:59:56 +1100

    Pre and Post purification filters

---
+++ src/Filters/Filter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EthicalJobs\Sanitize\Filters;
+
+/**
+ * HTML filter contract
+ * 
+ * @author Andrew McLagan <andrew@ethicaljobs.com.au>
+ */
+
+interface Filter
+{
+    /**
+     * Apply filter to raw html string
+     * 
+     * @param string $html
+     * @return string
+     */
+    public function apply(string $html): string;
+}
