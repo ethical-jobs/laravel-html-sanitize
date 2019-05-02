bca5fa7f3518184d36b8b958ae451fe7de70fda4
Dean Tedesco <dean@ethicaljobs.com.au>
2019-05-03 09:16:01 +1000

    Clean up of test code and implement data providers where possible.

--- tests/Unit/Filters/RemoveDoubleBreaksTest.php
+++ tests/Unit/Filters/RemoveDoubleBreaksTest.php
@@ -10,50 +10,49 @@ class RemoveDoubleBreaksTest extends TestCase
     /**
      * @test
      */
-    public function it_removes_multiple_breaks()
+    public function it_removes_multiple_breaks(): void
     {
         $html = '<h1>Hello world</h1><br /><br /><p>Hello there wonderful paragraph 1</p> <br /> <br /> <p>Hello there wonderful paragraph 2</p><br />   &nbsp;<br /><br /><br /><br /><br /><br><br><p>Hello there wonderful paragraph 3</p>';
+        $expected = '<h1>Hello world</h1><br><p>Hello there wonderful paragraph 1</p> <br><p>Hello there wonderful paragraph 2</p><br><p>Hello there wonderful paragraph 3</p>';
 
         $filter = new RemoveDoubleBreaks;
-
         $filtered = $filter->apply($html);
 
-        $this->assertEquals($filtered,
-            '<h1>Hello world</h1><br><p>Hello there wonderful paragraph 1</p> <br><p>Hello there wonderful paragraph 2</p><br><p>Hello there wonderful paragraph 3</p>');
+        $this->assertEquals($expected, $filtered);
     }
 
     /**
      * @test
      */
-    public function it_removes_breaks_of_differing_formats()
+    public function it_removes_breaks_of_differing_formats(): void
     {
         $html = '<h1>Hello world</h1><br /><br><br/><br>Hello<br  /><br><br>There';
+        $expected = '<h1>Hello world</h1><br>Hello<br>There';
 
         $filter = new RemoveDoubleBreaks;
-
         $filtered = $filter->apply($html);
 
-        $this->assertEquals($filtered, '<h1>Hello world</h1><br>Hello<br>There');
+        $this->assertEquals($expected, $filtered);
     }
 
     /**
      * @test
      */
-    public function it_removes_large_amounts_of_breaks()
+    public function it_removes_large_amounts_of_breaks(): void
     {
         $html = '<h1>Hello</h1><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br><br /><br><br/><br>There.';
+        $expected = '<h1>Hello</h1><br>There.';
 
         $filter = new RemoveDoubleBreaks;
-
         $filtered = $filter->apply($html);
 
-        $this->assertEquals($filtered, '<h1>Hello</h1><br>There.');
+        $this->assertEquals($expected, $filtered);
     }
 
     /**
      * @test
      */
-    public function it_can_account_for_new_lines()
+    public function it_can_account_for_new_lines(): void
     {
         $html = '<h1>Hello</h1>
         <br /><br><br/><br><br />
@@ -62,15 +61,13 @@ class RemoveDoubleBreaksTest extends TestCase
         Hello
         <br>
         There';
-
         $expected = '<h1>Hello</h1>
         <br>Hello
         <br>There';
 
         $filter = new RemoveDoubleBreaks;
-
         $filtered = $filter->apply($html);
 
-        $this->assertEquals($filtered, $expected);
+        $this->assertEquals($expected, $filtered);
     }
 }
--- tests/Unit/Purifier/ElementsTest.php
+++ tests/Unit/Purifier/ElementsTest.php
@@ -10,7 +10,7 @@ class ElementsTest extends TestCase
     /**
      * @test
      */
-    public function it_removes_invalid_elements()
+    public function it_removes_invalid_elements(): void
     {
         $html = '
             <h1>Hello Heading</h1>
@@ -41,7 +41,7 @@ class ElementsTest extends TestCase
     /**
      * @test
      */
-    public function it_only_allows_style_attribute_on_paragraphs()
+    public function it_only_allows_style_attribute_on_paragraphs(): void
     {
         $html = '
             <h1 style="text-align: right;">Hello Heading</h1>
@@ -76,7 +76,7 @@ class ElementsTest extends TestCase
     /**
      * @test
      */
-    public function it_converts_all_heading_tags_to_H3()
+    public function it_converts_all_heading_tags_to_H3(): void
     {
         $html = '
             <h1>Heading #1</h1>
@@ -116,7 +116,7 @@ class ElementsTest extends TestCase
     /**
      * @test
      */
-    public function it_removes_double_linebreak_elements()
+    public function it_removes_double_linebreak_elements(): void
     {
         $html = '<h1>Hello world</h1>
             <br /><br />
@@ -139,7 +139,7 @@ class ElementsTest extends TestCase
     /**
      * @test
      */
-    public function it_leaves_single_linebreak_elements()
+    public function it_leaves_single_linebreak_elements(): void
     {
         $html = '
             <h1>Hello world</h1>
