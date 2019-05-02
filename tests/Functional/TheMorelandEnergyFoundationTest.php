0153de1ae7b167905cbc7dd378bb016ff4527e84
Dean Tedesco <dean@ethicaljobs.com.au>
2019-05-03 09:19:04 +1000

    trello-bug-374 Added Moreland Foundation functional test to test the input of data from a Word document.

---
+++ tests/Functional/TheMorelandEnergyFoundationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Functional;
+
+use Tests\Fixtures\LoadsFixtures;
+use Tests\TestCase;
+
+class TheMorelandEnergyFoundationTest extends TestCase
+{
+    use LoadsFixtures, Purifiable;
+}
